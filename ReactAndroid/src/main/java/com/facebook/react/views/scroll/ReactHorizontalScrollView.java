e798dd7b7c152ee0635861cea7dead96ea8c5a18
Leo Nikkilä <hello@lnikki.la>
2018-09-25 14:08:01 +0300

    Fix Android momentum scroll differences to iOS
    - Momentum scroll events should not be emitted when there's no momentum
    - Momentum scroll should be interrupted by a drag, which should not emit
      an onMomentumScrollEnd event. Instead a new momentum scroll should
      begin once dragging ends.

--- ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactHorizontalScrollView.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactHorizontalScrollView.java
@@ -223,6 +223,12 @@ public class ReactHorizontalScrollView extends HorizontalScrollView implements
         ReactScrollViewHelper.emitScrollBeginDragEvent(this);
         mDragging = true;
         enableFpsListener();
+        // The user might be interrupting a fling/snap and we need to cancel any ongoing animations.
+        // Mirrors behaviour on iOS by starting a new momentum scroll event series after this drag.
+        if (mPostTouchRunnable != null) {
+          removeCallbacks(mPostTouchRunnable);
+          mPostTouchRunnable = null;
+        }
         return true;
       }
     } catch (IllegalArgumentException e) {
@@ -409,6 +415,12 @@ public class ReactHorizontalScrollView extends HorizontalScrollView implements
   private void handlePostTouchScrolling(int velocityX, int velocityY) {
     // If we aren't going to do anything (send events or snap to page), we can early exit out.
     if (!mSendMomentumEvents && !mPagingEnabled && !isScrollPerfLoggingEnabled()) {
+      return;
+    }
+
+    // Don't emit momentum events if there's not going to be a momentum scroll, mirrors iOS momentum
+    // scroll behaviour.
+    if (!mPagingEnabled && velocityX == 0 && velocityY == 0) {
       return;
     }
 
