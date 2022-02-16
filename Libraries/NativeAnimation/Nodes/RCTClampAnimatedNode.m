06a53e43219501f88ca1a39c52cea3a43d5114d3
Erick <esoto@blvd.co>
2022-02-16 08:25:01 -0800

    update RCTClampAnimatedNode.h import

--- Libraries/NativeAnimation/Nodes/RCTClampAnimatedNode.m
+++ Libraries/NativeAnimation/Nodes/RCTClampAnimatedNode.m
@@ -7,7 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-#import "RCTClampAnimatedNode.h"
+#import <React/RCTClampAnimatedNode.h>
 
 @implementation RCTClampAnimatedNode
 {
